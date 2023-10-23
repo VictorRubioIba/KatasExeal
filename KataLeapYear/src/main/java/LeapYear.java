ce2bec87b8816f2e2f5e11734850d14bacaaa826
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2023-10-23 19:24:37 +0200

    refactor: add method divisibleBy400

--- KataLeapYear/src/main/java/LeapYear.java
+++ KataLeapYear/src/main/java/LeapYear.java
@@ -1,12 +1,16 @@
 public class LeapYear {
 
   public boolean leapYear(int year) {
-    boolean leapYear = year % 400 == 0;
+    boolean leapYear = divisibleBy400(year);
 
-    if (year % 100 == 0 && year % 400 != 0) {
+    if (year % 100 == 0 && !divisibleBy400(year)) {
       leapYear = false;
     }
 
     return leapYear;
+  }
+
+  private boolean divisibleBy400(int year) {
+    return year % 400 == 0;
   }
 }
